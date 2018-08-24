ae351c0242d491217ad46afca154d60d2a7d98ea
Quansheng.Xiong <Quansheng.Xiong@fdmgroup.com>
2018-08-24 10:05:13 -0400

    added JP parsing

--- src/model/card/CardType.java
+++ src/model/card/CardType.java
@@ -1,5 +1,18 @@
 package model.card;
 
 public enum CardType {
-	Character, Event, Climax
+	CHARACTER, EVENT, CLIMAX;
+	
+	public static CardType parseJP(String text){
+		switch (text){
+		case "クライマックス":
+			return CLIMAX;
+		case "キャラ":
+			return CHARACTER;
+		case "イベント":
+			return EVENT;
+			default:
+				throw new IllegalArgumentException(text);
+		}
+	}
 }
