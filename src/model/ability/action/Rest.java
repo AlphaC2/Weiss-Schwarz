d72474b02d2d41e864a339a20ab6ac5cae94b1ee
Quansheng.Xiong <Quansheng.Xiong@fdmgroup.com>
2018-09-18 14:33:38 -0400

    fix rest

--- src/model/ability/action/Rest.java
+++ src/model/ability/action/Rest.java
@@ -18,7 +18,7 @@ public class Rest extends Action<Slot> {
 
 	@Override
 	protected void setTargets(PlayerController p1, PlayerController p2) {
-		targets = p1.getBoard().getStage().getSlots();
+		targets.addAll(p1.getBoard().getStage().getSlots());
 	}
 
 	@Override
