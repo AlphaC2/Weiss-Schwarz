71966d3e9aec1f2469bd220767c07b9b2ed04121
Quansheng.Xiong <Quansheng.Xiong@fdmgroup.com>
2018-09-20 14:57:22 -0400

    added other to self

--- src/model/ability/action/condition/Self.java
+++ src/model/ability/action/condition/Self.java
@@ -5,15 +5,21 @@ import model.card.Card;
 public class Self extends Condition<Card>{
 
 	private Card card;
+	private boolean other;
 	
 	protected Self(Card card) {
+		this(card, false);
+	}
+	
+	protected Self(Card card, boolean other) {
 		super("Self");
 		this.card = card;
+		this.other = other;
 	}
 
 	@Override
 	public boolean check() {
-		return card.equals(target);
+		return !other && card == target;
 	}
 
 }
